85efcacdee0eaa8b14d967e8f6e1dd7745e02710
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-04-03 13:55:31 +0300

    show_scores(function) + tests -- ready

--- play_round.py
+++ play_round.py
@@ -17,3 +17,14 @@ def choose_winner(test_input=None):
         print("Wrong input. Please try again.")
         return choose_winner(None)
     return "wrong_input"
+
+def show_scores(round_num, player_one, player_two, testing=False):
+    if not testing:
+        print("".join( [ "==== ", "Round ", str(round_num), " score ===="]))
+        print(" ".join(["Player 1 -- ", player_one.get_score()]))
+        print(" ".join(["Player 2 -- ", player_two.get_score()]))
+    output_str = "".join(["==== ", "Round ", str(round_num), " score ====\n",
+                          "Player 1 -- ", player_one.get_score(), "\n",
+                          "Player 2 -- ", player_two.get_score() 
+                        ])
+    return output_str
--- play_round_test.py
+++ play_round_test.py
@@ -2,13 +2,24 @@ import unittest
 from play_round import *
 from player_class import *
 
-class TestStage2(unittest.TestCase):
+class TestStage3(unittest.TestCase):
 
     def test_choose_winner(self):
         self.assertEqual(choose_winner("1"), 1)
         self.assertEqual(choose_winner("2"), 2)
         self.assertEqual(choose_winner("0"), 0)
         self.assertEqual(choose_winner(""), "wrong_input")
+
+    def test_show_scores(self):
+        player_one = Player("20")
+        player_two = Player("30")
+        round_num = 2
+        testing_on = True
+        correct_output = "".join(["==== Round 2 score ====\n",
+                                  "Player 1 -- 20\n",
+                                   "Player 2 -- 30" ])
+        test_output = show_scores(round_num, player_one, player_two, testing_on)
+        self.assertEqual(test_output, correct_output)
 
 if __name__=="__main__":
     unittest.main()
