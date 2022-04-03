1a8eadf06eab20010c661538a41278bd2233e785
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-04-03 21:43:59 +0300

    enter_menu() update

--- enter_menu.py
+++ enter_menu.py
@@ -1,18 +1,19 @@
-def print_menu(menu_templ_file):
-    with open(menu_templ_file, 'r') as file:
-        for line in file:
-            print(line.strip())
-
-def get_user_choice():
-    print("Please enter the number: ")
-    user_choice = str(input())
-    if user_choice == "1":
-        return 1
-    if user_choice == "0":
-        return 0
-    print("Wrong input. Please try again.")
-    return get_user_choice()
-
 def enter_menu(menu_templ_file):
+
+    def print_menu(menu_templ_file):
+        with open(menu_templ_file, 'r') as file:
+            for line in file:
+                print(line.strip())
+    
+    def get_user_choice():
+        print("Please enter the number: ")
+        user_choice = str(input())
+        if user_choice == "1":
+            return 1
+        if user_choice == "0":
+            return 0
+        print("Wrong input. Please try again.")
+        return get_user_choice()
+
     print_menu(menu_templ_file)
     return get_user_choice()
