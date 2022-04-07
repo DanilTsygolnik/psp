4459e65d49a423d67ca4c05218cedeeaaaee882c
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-04-07 20:53:35 +0300

    step 1.3: fix 9-обнаруж-80 -> 9-устран-80

--- parse_input_test.py
+++ parse_input_test.py
@@ -1,41 +1,50 @@
 import unittest
 from parse_input import *
 
-class TestParseInput(unittest.TestCase):
+class TestGetChartDimensions(unittest.TestCase):
+    ''' dev step 1.1'''
 
-    def test_get_chart_sizes(self):
-        test_output = get_chart_sizes("task_input.txt")
-        correct_output = {'height': 4, 'width': 8}
-        self.assertEqual(test_output, correct_output)
-
+    def test_empty_input(self):
         empty_file_output = get_chart_sizes("empty_input.txt")
         correct_output = 0
         self.assertEqual(empty_file_output, correct_output)
 
-    def test_search_alive_in_string(self):
+    def test_standard_input(self):
+        test_output = get_chart_sizes("task_input.txt")
+        correct_output = {'height': 4, 'width': 8}
+        self.assertEqual(test_output, correct_output)
+
+class TestSearchAliveInString(unittest.TestCase):
+
+    def test_no_alive_cells_in_string(self):
         string_num = 3
         input_string = "......"
         test_output = search_alive_in_string(string_num, input_string)
         correct_output = []
         self.assertEqual(test_output, correct_output)
 
+    def test_alive_cells_in_string(self):
         string_num = 3
         input_string = "...*..*..*"
         test_output = search_alive_in_string(string_num, input_string)
         correct_output = ["h3w4", "h3w7", "h3w10"]
         self.assertEqual(test_output, correct_output)
 
-    def test_get_alive_cells_ids(self):
-        input_file = "task_input.txt"
+class TestGetAliveCellsIds(unittest.TestCase):
 
+    def setUp(self):
+        self.input_file = "task_input.txt"
+
+    def test_input_type_checking(self):
         with self.assertRaises(TypeError):
-            get_alive_cells_ids(0, input_file)
+            get_alive_cells_ids(0, self.input_file)
 
-        test_output = get_alive_cells_ids({}, input_file)
+    def test_get_alive_cells_ids(self):
+        test_output = get_alive_cells_ids({}, self.input_file)
         correct_output = {'alive_cells_ids':["h2w5", "h3w4", "h3w5"]}
         self.assertEqual(test_output, correct_output)
 
-class TestParse(unittest.TestCase):
+class TestParseInput(unittest.TestCase):
 
     def test_main(self):
         input_file = "task_input.txt"
